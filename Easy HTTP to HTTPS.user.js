878171326ac6ea4e8486b6d86d68155d4379421c
Cybo1927 <39284087+Cybo1927@users.noreply.github.com>
2020-02-02 12:46:26 -0600

    Update Easy HTTP to HTTPS.user.js

--- Easy HTTP to HTTPS.user.js
+++ Easy HTTP to HTTPS.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Easy HTTP to HTTPS
-// @version      1.1
+// @version      1.2
 // @homepage     https://github.com/Cybo1927/Userscripts/blob/master/Easy%20HTTP%20to%20HTTPS.user.js
 // @description  Redirect HTTP to HTTPS
 // @author       Cybo1927
 // @run-at       document-start
 // @include      http://*/*
+// @exclude      http://127.0.0.1/*
 // @grant        none
 // ==/UserScript==
 
