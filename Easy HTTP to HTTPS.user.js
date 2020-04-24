252a7516587f0ca1c77849569192497431d527ac
InnoScorpio <29800567+InnoScorpio@users.noreply.github.com>
2020-04-24 15:45:53 +0000

    Add homepage

--- Easy HTTP to HTTPS.user.js
+++ Easy HTTP to HTTPS.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Easy HTTP to HTTPS
 // @version      04.24.2020
-// @homepage     
+// @homepage     https://github.com/InnoScorpio/Userscripts/raw/master/Easy%20HTTP%20to%20HTTPS.user.js
 // @description  Redirect HTTP to HTTPS
 // @author       InnoScorpio
 // @run-at       document-start
